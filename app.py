66cfeecf3f3b21c9e3f7e822f3d76ba5af831568
herbert zapata <herbert.zapata19@gmail.com>
2022-03-15 00:20:14 -0300

    cambios

--- app.py
+++ app.py
@@ -1,22 +1,46 @@
-from flask import Flask, render_template, jsonify, request
-from flaskext.mysql import MySQL
-from flask_cors import CORS, cross_origin
+#from flask import Flask, render_template, jsonify, request
+from flask import Flask
+from flask import jsonify
+from flask import request
+from flask_cors import CORS
+#cross_origin
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+from flask_jwt_extended import JWTManager
 
 app = Flask(__name__)
 
-# Mysql Connection
-app.config['MYSQL_HOST'] = 'localhost' 
-app.config['MYSQL_USER'] = 'bigbuda_herbert_zap'
-app.config['MYSQL_PASSWORD'] = '62032597Hz.#'
-app.config['MYSQL_DB'] = 'bigbuda_export_json'
-#mysql = MySQL(app) 
-
-
+# Setup the Flask-JWT-Extended extension
+app.config["JWT_SECRET_KEY"] = "bigbuda2022"
+jwt = JWTManager(app)
 
 
 #cors validate dominios o ip
 CORS(app,resource={r"/pedidos/*":{"origins":"http://localhost"}})
 from pedidos import products
+
+
+# Create a route to authenticate your users and return JWTs. The
+# create_access_token() function is used to actually generate the JWT.
+@app.route("/login", methods=["POST"])
+def login():
+    username = request.json.get("username", None)
+    password = request.json.get("password", None)
+    if username != "test" or password != "test":
+        return jsonify({"msg": "Bad username or password"}), 401
+
+    access_token = create_access_token(identity=username)
+    return jsonify(access_token=access_token)
+
+@app.route("/protected", methods=["GET"])
+@jwt_required()
+def protected():
+    # Access the identity of the current user with get_jwt_identity
+    current_user = get_jwt_identity()
+    return jsonify(logged_in_as=current_user), 200
+
+
 
 #busqueda@cross_origin
 @app.route("/")
@@ -24,7 +48,7 @@ def index():
   return render_template('index.html')
     
 #ver todos los pedidos exportados
-@app.route('/')
+@app.route('/pedidos')
 def get_pedidos():
     return jsonify({"pedidos": products})
 
